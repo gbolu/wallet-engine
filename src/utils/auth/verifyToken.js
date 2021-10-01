4224b6a9c51a79fc1b54aa8cde7393987ce800f9
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2021-10-01 14:28:41 +0100

    Modify: Remove unnecessary tools

--- src/utils/auth/verifyToken.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { JWT_HASH } = require("../../config");
-
-const verifyToken = async (token) => jwt.verify(token, JWT_HASH);
-
-module.exports = verifyToken;
